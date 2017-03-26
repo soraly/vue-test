351d3fbd2e71147b6d845215159fb97530bc9818
lzx <1400415006@qq.com>
2017-03-26 22:31:39 +0800

    vue11111

---
+++ php/11.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<?php echo "<p>Hello World</p>"; ?><br/>
+this is  index.php page
+</body>
+</html>
---
+++ php/data.php
@@ -0,0 +1,6 @@
+<?php
+    echo 666;
+    $a = $_GET['a'];
+    $b = $_GET['b'];
+    echo $a + $b;
+?>
