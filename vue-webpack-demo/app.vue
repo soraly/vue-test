31d4ab3454d8edd60ce5fb15c148d7e84d08a913
lzx <1400415006@qq.com>
2017-03-30 22:39:54 +0800

    vue event bus

--- vue-webpack-demo/app.vue
+++ vue-webpack-demo/app.vue
@@ -1,15 +1,40 @@
 <template>
     <div>
+        <hhh :childata="word" :child2="word2"></hhh>
         <h3>this is vue</h3>
-        <div>hello , {{name}}</div>
+        <div class="name" >hello , {{name}}</div>
+        <div>父元素的数据 {{word.content}}</div>
+
     </div>
 </template>
 <script>
+    import head from './components/head.vue';
+    import Vue from 'vue/dist/vue';
+
+
     export default {
+        beforeCreate(){
+            console.log(111,'33');
+            Vue.component('Hom',{
+                template: '<div>this is home</div>'
+            })
+        },
         data(){
             return {
-                name: 'xiang'
+                name: 'xiang',
+                word: {
+                    content: 'we will be better'
+                },
+                word2: 'this is parent 的 word2'
             }
+        },
+        components: {
+            "hhh": head
         }
     }
 </script>
+<style>
+    .name {
+        background: skyblue;
+    }
+</style>
---
+++ vue-webpack-demo/components/head.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="head">
+        <h3>this is head</h3>
+        <div>hello {{name}}</div>
+        <div>父元素传来的数据是 {{childata.content}}</div>
+        <p>还有{{children}}</p>
+        <button @click="change()" class="btn btn-parimay">点我</button>
+
+    </div>
+</template>
+<script>
+
+    export default {
+        props: ['childata','child2'],
+        data(){
+            return {
+                'children': this.child2,
+                'name': '小芬芬'
+            }
+        },
+        components: {
+        },
+        mounted(){
+
+        },
+        methods: {
+            change(){
+                this.childata.content = 'heheheheh';
+                this.children = '变化啦'
+                console.log(this.childata.content,'22')
+            }
+        },
+        computed:{
+
+        }
+    }
+</script>
+<style scoped="">
+    .head {
+        background: deepskyblue;
+    }
+</style>
